7c4f83ae8b3e556ce91c661f672bf89d15b64e53
Brako Roncoroni <kobradevelopment@outlook.com>
2025-11-24 22:01:09 -0300

    Actualizaciones v1.0

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const app = express();
 const routes = require("./routes/index");
+const path = require('path');
+
+
+const handlebars = require("express-handlebars");
 const { paths } = require ("./config/config");
+
+//Seteo y configuracion de Handlebars
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, "views/layouts"),
+    partialsDir: path.join(__dirname, "views/partials"),
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", paths.views);
+console.log("------>", paths.views);
+
 
 
 app.use(express.json());
@@ -15,13 +34,9 @@ app.use("/public", express.static(paths.public));
 console.log("---------->", paths.public); //Consologueo para probar si esta llegando correctamente el path;
 
 app.get('/', (req, res) => {
-  res.render("pages/home", {})
+ return res.render("pages/home", {})
 }); 
 
-
-
- 
- 
 
 
 app.get('/', (req, res) => {
--- src/config/config.js
+++ src/config/config.js
@@ -4,5 +4,6 @@ module.exports = {
     PORT: 3030,
     paths: {
         public: path.join (__dirname, '../../public'),
+        views: path.join(__dirname, '../../src/views')
     },
 };
